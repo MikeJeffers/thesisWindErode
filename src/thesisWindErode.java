0042ecd14a9db3e05e456e6cfc4de9e614045b6d
Jordan C. Parsons <jordancparsons@gmail.com>
2012-10-19 03:49:43 -0400

    Second Commit, from laptop.

--- src/thesisWindErode.java
+++ src/thesisWindErode.java
@@ -1,16 +1,49 @@
+import peasy.PeasyCam;
 import processing.core.*;
+import toxi.processing.ToxiclibsSupport;
 
 
 public class thesisWindErode extends processing.core.PApplet {
 	private static final long serialVersionUID = 1L;
 
-	public void setup(){
-		
-		println("Hello World");
+	// General
+	ToxiclibsSupport gfx;
+	PeasyCam nav; // cam
+	PFont font;
+
+
+	// Boolean
+	boolean paused = false;
+	
+
+	public void setup() {
+		size(1280, 720, OPENGL);
+		smooth();
+		frameRate(30);
+
+		font = createFont("Arial Bold", 48);
+		gfx = new ToxiclibsSupport(this);
+
+		nav = new PeasyCam(this, 500);
+		nav.setMinimumDistance(-50);
+
 	}
 	
-	public void draw(){
-		
+	public void update() {
+
+	}
+
+	public void draw() {
+		background(50);
+
+		if (!paused)
+			update();
+
+
+		nav.beginHUD();
+		fill(0);
+		text(frameRate, 20, 20);
+		nav.endHUD();
 	}
 	
 }
